a9cead3bde5582ba82c432bc6eaaead2ff120eec
Samuel Palacio Gaviria <sapalaciog@unal.edu.co>
2025-11-25 21:16:25 -0500

    Add files via upload

---
+++ Act5_Ej1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.act5_ej1;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Act5_Ej1 {
+
+    public static void main(String[] args) {
+        VPrincipal formulario = new VPrincipal();
+        formulario.setVisible(true);
+        
+    }
+}
